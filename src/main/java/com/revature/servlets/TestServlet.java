cf9fd0042eaeda90eb4f285b1c6bd3ddf530f2db
Tucker <tramitchell100@gmail.com>
2019-06-07 15:54:39 -0400

    uri

--- src/main/java/com/revature/servlets/TestServlet.java
+++ src/main/java/com/revature/servlets/TestServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/*test")
+@WebServlet("/test/*")
 public class TestServlet extends HttpServlet {
 
 	@Override
